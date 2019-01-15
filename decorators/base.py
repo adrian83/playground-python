6d0915f8f5b3745c015df7a42c31934ebab4d29e
adrian83 <adrianbrzoza1983@gmail.com>
2019-01-15 18:33:43 +0100

    decorators - minor update

--- decorators/base.py
+++ decorators/base.py
@@ -2,21 +2,12 @@ import types
 import time
 
 info_format = "Executing '{0}' with params: {1} {2}"
-time_format = "Execution of '{0}' took {1} millisecond(s)"
-
-
-now_in_ms = lambda: int(round(time.time() * 1000))
-
 
 def func_invocation_info(call, *args, **kwargs):
     nameable = call if isinstance(call, types.FunctionType) else call.__class__
     argsStr = ", ".join(args)
     kwargsStr = ", ".join(["{0}={1}".format(k, v) for k, v in kwargs.items()])
-    start = now_in_ms()
-    argsInfo = info_format.format(nameable.__name__, argsStr, kwargsStr)
-    end = now_in_ms()
-    timeInfo = time_format.format(nameable.__name__, end-start)
-    return "{0}\n{1}".format(argsInfo, timeInfo)
+    return info_format.format(nameable.__name__, argsStr, kwargsStr)
 
 
 def introduce(first_name, last_name, **info):
